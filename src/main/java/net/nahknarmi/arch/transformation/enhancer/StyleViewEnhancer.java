bb5f25d3f6eb77c159367ab5e7b2385f269cc1ad
Roy van den Heuvel <rvandenheuvel@voogd.com>
2020-03-06 12:49:53 +0100

    Personalised styling

--- src/main/java/net/nahknarmi/arch/transformation/enhancer/StyleViewEnhancer.java
+++ src/main/java/net/nahknarmi/arch/transformation/enhancer/StyleViewEnhancer.java
@@ -17,5 +17,10 @@ public class StyleViewEnhancer implements WorkspaceEnhancer {
         styles.addElementStyle(Tags.SOFTWARE_SYSTEM).background("#1168bd").color("#ffffff");
         styles.addElementStyle(Tags.PERSON).background("#08427b").color("#ffffff").shape(Shape.Person);
         styles.addElementStyle(Tags.CONTAINER).background("#73f275").color("#ffffff");
+        styles.addElementStyle("Database").background("#73f275").color("#ffffff").shape(Shape.Cylinder);
+        styles.addElementStyle("Folder").background("#73f275").color("#ffffff").shape(Shape.Folder);
+        styles.addElementStyle("App").background("#73f275").color("#ffffff").shape(Shape.MobileDevicePortrait);
+        styles.addElementStyle("Website").background("#73f275").color("#ffffff").shape(Shape.WebBrowser);
+        styles.addElementStyle("Extern").background("#ff8080").color("#ffffff").shape(Shape.WebBrowser);
     }
 }
